8d45258921bafcd6021cd5cac58c1657da7b40d6
lanyuqiancong <69624701+lanyuqiancong@users.noreply.github.com>
2020-09-14 14:37:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const signInButton = document.querySelector('#signIn');
 const signUpButton = document.querySelector('#signUp');
 signUpButton.addEventListener('click', () => container.classList.add
 ('right-panel-active'));
-signInButton.addEventListener('click', () => container.classList.remove
-('right-panel-active'));
+signInButton.onclick = function () {
+    self.location = 'https://qiancong.me/reward/';
+}
